a6ce9c6293db2ff80c65b762ad205bbbce83fe68
dav <dav@mikucare.com>
2023-12-25 23:24:07 -0500

    training is now consuming previous datapoint, but is no longer MSE towards target color. also enables tensorboard logging of loss combiner class

--- octo_model.py
+++ octo_model.py
@@ -11,10 +11,8 @@ from tensorflow import keras
 from simulator.octo_datagen import OctoDatagen
 from OctoConfig import GameParameters
 from util import (
-    sucker_model_data_constructor, 
-    WeightedSumLoss, 
-    octo_norm, 
-    train_test_split, 
+    WeightedSumLoss,
+    train_test_split,
     convert_pytype_to_model_input_type
 )
 
@@ -26,9 +24,9 @@ SAVE_DATA_TO_DISK = True
 RESTORE_DATA_FROM_DISK = True
 RUN_TRAINING = True
 GENERATE_TENSORBOARD = True
-SAVE_MODEL_TO_DISK = False
+SAVE_MODEL_TO_DISK = True
 
-RESTORE_MODEL_FROM_DISK = False
+RESTORE_MODEL_FROM_DISK = True
 RUN_INFERENCE = True
 
 RUN_EVAL = False
@@ -49,6 +47,7 @@ if RUN_DATAGEN:
 # %% Model training
 if RUN_TRAINING:
     tf.config.run_functions_eagerly(False)
+    batch_size = GameParameters['batch_size']
     if RESTORE_DATA_FROM_DISK:
         del data
         with open('datagen/sucker_data.pkl', 'rb') as file:
@@ -58,26 +57,27 @@ if RUN_TRAINING:
     constraint_loss_weight = GameParameters['constraint_loss_weight']
     max_hue_change = GameParameters['octo_max_hue_change']
     scaler = preprocessing.MinMaxScaler()
-    state_data = np.array([data['state_data']]) #sucker's current color
-    gt_data = np.array([data['gt_data']]) #sucker's ground truth
-    state_data_norm = np.array(list(map(octo_norm, state_data)))
-    gt_data_norm = np.array(list(map(octo_norm, gt_data)))
-    train_data, train_labels, val_data, val_labels = train_test_split(state_data_norm,
-                                                                      gt_data_norm)
+    state_data = np.array([data['state_data']], dtype='float32') #sucker's current color
+    gt_data = np.array([data['gt_data']], dtype='float32') #sucker's ground truth
 
-    train_input_data, train_gt_data, train_orig_data = sucker_model_data_constructor(train_data, train_labels)
-    val_input_data, val_gt_data, val_orig_data = sucker_model_data_constructor(val_data, val_labels)
+    train_data, train_labels, val_data, val_labels = train_test_split(state_data, gt_data)
+
+    train_io_data = convert_pytype_to_model_input_type(np.transpose(np.stack((train_data, train_labels))))
+    val_io_data = convert_pytype_to_model_input_type(np.transpose(np.stack((val_data, val_labels))))
+
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_io_data, train_io_data))
+    val_dataset = tf.data.Dataset.from_tensor_slices((val_io_data, val_io_data))
 
     sucker_model = keras.Sequential()
-    sucker_model.add(keras.layers.Dense(units=3, input_dim=2, activation="relu", name="hidden_layer1"))
+    inp = keras.layers.Input(shape=(1,))
+    outp = keras.layers.Dense(units=1, activation="tanh", name="prediction")
+
+    sucker_model.add(inp)
+    sucker_model.add(keras.layers.Dense(units=3, activation="relu", name="hidden_layer1"))
     sucker_model.add(keras.layers.Dense(units=3, activation="relu", name="hidden_layer2"))
     sucker_model.add(keras.layers.Dense(units=3, activation="relu", name="hidden_layer3"))
-    sucker_model.add(keras.layers.Dense(units=1, activation="tanh", name="prediction"))
+    sucker_model.add(outp)
 
-    losses = [
-        WeightedSumLoss(original_values=train_orig_data,
-                       threshold=max_hue_change,
-                       weight=constraint_loss_weight)]
     # Tensorboard configuration. To start tensorboard, use:
     # tensorboard serve --logdir <log directory>
     callbacks = []
@@ -86,33 +86,60 @@ if RUN_TRAINING:
         tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
         callbacks.append(tensorboard_callback)
 
-    sucker_model.compile(optimizer="sgd",
-                loss=losses,
-                metrics=["mse"])
+    ### Custom training loop
+    optimizer = keras.optimizers.SGD(learning_rate=1e-3)
+    logwriter = tf.summary.create_file_writer('logs')
 
-    sucker_model.fit(x=train_input_data,
-            y=train_gt_data,
-            epochs=GameParameters['epochs'],
-            batch_size=GameParameters['batch_size'],
-            callbacks=callbacks)
+    epochs = 10
+    for epoch in range(epochs):
+        print("\nStart of epoch %d" % (epoch,))
+
+        # Iterate over the batches of the dataset.
+        for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
+            # Open a GradientTape to record the operations run
+            # during the forward pass, which enables auto-differentiation.
+            with tf.GradientTape() as tape:
+                # Run the forward pass of the layer.
+                # The operations that the layer applies
+                # to its inputs are going to be recorded
+                # on the GradientTape.
+                logits = sucker_model(x_batch_train, training=True)  # Logits for this minibatch
+
+                # Redefine the loss function each step in order to track the step in the logs
+                loss_fn = WeightedSumLoss(threshold=max_hue_change,
+                              weight=constraint_loss_weight,
+                              step = step,
+                              logwriter=logwriter)
+
+                # Compute the loss value for this minibatch.
+                loss_value = loss_fn(y_batch_train, logits)
+
+            # Use the gradient tape to automatically retrieve
+            # the gradients of the trainable variables with respect to the loss.
+            grads = tape.gradient(loss_value, sucker_model.trainable_weights)
+
+            # Run one step of gradient descent by updating
+            # the value of the variables to minimize the loss.
+            optimizer.apply_gradients(zip(grads, sucker_model.trainable_weights))
+
+            # Log every 200 batches.
+            if step % 200 == 0:
+                print(
+                    "Training loss (for one batch) at step %d: %.4f"
+                    % (step, float(loss_value))
+                )
+                print("Seen so far: %s samples" % ((step + 1) * batch_size))
 
     if GENERATE_TENSORBOARD:
         print(f"Tensorboard generated, run with:\n\n\ttensorboard serve --logdir {log_dir}\n")
 
-    # These need to change because now our WeightedSumLos original value should be the val set 
+    # These need to change because now our WeightedSumLos original value should be the val set
     eval_losses = [
-        WeightedSumLoss(original_values=val_orig_data,
-                       threshold=max_hue_change,
+        WeightedSumLoss(threshold=max_hue_change,
                        weight=constraint_loss_weight)]
     sucker_model.compile(optimizer="sgd",
                 loss=eval_losses,
                 metrics=["mse"])
-    loss, accuracy = sucker_model.evaluate(x=val_input_data,
-                                           y=val_gt_data,
-                                           batch_size=GameParameters['batch_size'],
-                                           callbacks=callbacks)
-
-    print(f"Loss: {loss:.3f}, Accuracy: {accuracy:.3f}")
     print(f"Model training completed at time t={tm.time() - start:.3f}")
 
     # %% Model deployment (this is only run if a new model was successfully trained)
@@ -130,33 +157,32 @@ if RUN_INFERENCE:
     assert sucker_model, "No model found, can't run inference"
     range_vals = [0.0,0.25,0.5,0.75,1.0]
     res = []
-    for curr in map(octo_norm, range_vals):
+    for curr in range_vals:
         row = []
-        for gt in map(octo_norm, range_vals):
-            test_input = np.array([[curr, gt]])
+        for gt in range_vals:
+            test_input = np.array([curr, gt])
 
             #computes prediction output
             pred = sucker_model.predict(test_input, verbose = 0)[0][0]
             row.append(pred)
 
             #computes loss
-            original_value = convert_pytype_to_model_input_type(curr)
             gt_inference_input = convert_pytype_to_model_input_type(gt)
             pred_inference_input = convert_pytype_to_model_input_type(pred)
 
-            inference_losses = [WeightedSumLoss(original_values=original_value,
-                                    threshold=max_hue_change,
+            inference_losses = [WeightedSumLoss(threshold=max_hue_change,
                                     weight=constraint_loss_weight)]
 
             loss_str = ""
-            for loss_func in inference_losses:
-                with tf.GradientTape() as tape:
-                    loss = loss_func(gt_inference_input, pred_inference_input)
-                gradients = tape.gradient(loss, tf.constant(pred))
-                loss_str += f"\t{float(loss):.3f} \t<𝝳={gradients}>"
+            # for loss_func in inference_losses:
+            #     with tf.GradientTape() as tape:
+            #         loss, loss_details = loss_func(gt_inference_input, pred_inference_input)
+            #         print(loss_details)
+            #     gradients = tape.gradient(loss, tf.constant(pred))
+            #     loss_str += f"\t{float(loss):.3f} \t<𝝳={gradients}>"
 
             print(f"{curr:.2f}, \t{gt:.2f} -> \t{pred:.3f} \t(losses = {loss_str})")
-        res.append(octo_norm(row, True))
+        res.append(row)
 
     #plots out the results
     plt.figure(figsize = (10,7))
@@ -175,6 +201,13 @@ if RUN_INFERENCE:
 if RUN_EVAL:
     # For sucker color, eval is defined as the average of RMS of the RGB values
     # mean([rms(pred, gt) for each (pred, gt) in octopus])
+    loss, accuracy = sucker_model.evaluate(x=tf.convert_to_tensor(val_data, dtype='float32'),
+                                           y=tf.convert_to_tensor([val_labels], dtype='float32'),
+                                           batch_size=GameParameters['batch_size'],
+                                           callbacks=callbacks)
+
+    print(f"Loss: {loss:.3f}, Accuracy: {accuracy:.3f}")
+
     print(f"Model eval completed at time t={tm.time() - start:.3f}")
 
 # %% End and cleanup
--- util.py
+++ util.py
@@ -57,7 +57,7 @@ def train_test_split(data, labels, test_size=0.2, random_state=None):
 
     return train_data, train_labels, test_data, test_labels
 
-def sucker_model_data_constructor(data, labels):
+def sucker_model_data_constructor(data, labels, batch_size):
     """Convert and configure data types for training:
     y_true: Ground truth values. shape = `[batch_size, d0, .. dN]`
     y_pred: The predicted values. shape = `[batch_size, d0, .. dN]`
@@ -85,29 +85,32 @@ class ConstraintLoss(tf.keras.losses.Loss):
     We have defined the color change rate to be a maximum of 0.25 per iteration.
     This means that there is a cost incurred if the predicted value is 0.25
     greater than or less than the original value. The GT value is never used. """
-    def __init__(self, original_values, threshold=0.25, weight=1.0):
+    def __init__(self, threshold=0.25, weight=1.0):
         super().__init__()
 
         # The original_values tensor should have a 2D shape of (batch_size, num_features)
-        assert tf.is_tensor(original_values), "error: original values must be a tensor"
-        ov_size = original_values.shape
-        assert len(ov_size) == 2, "error: original value must have two dimensions"
-        batch_size = ov_size[0]
-        num_features = ov_size[1]
-        assert num_features == 1, "error: The original_values tensor should have a 2D shape of (batch_size = N, num_features = 1)"
-        self.original_values = original_values
+        # assert tf.is_tensor(original_values), "error: original values must be a tensor"
+        # ov_size = original_values.shape
+        # assert len(ov_size) == 2, "error: original value must have two dimensions"
+        # batch_size = ov_size[0]
+        # num_features = ov_size[1]
+        # assert num_features == 1, "error: The original_values tensor should have a 2D shape of (batch_size = N, num_features = 1)"
         self.threshold = threshold
         self.weight = float(weight)
 
-    def call(self, y_true, y_pred):
-        
+    def call(self, y_true: tf.TensorArray, y_pred: tf.TensorArray):
+
         # Calculate absolute difference between predictions and original values
         assert tf.is_tensor(y_pred) and tf.is_tensor(y_true), "error: inputs must be tensors"
 
-        diff = tf.abs(y_pred - self.original_values)
+        # the ground truth input is y_true[0] and is not important for this loss
+        original_value = y_true[1]
+        predicted_value = y_pred[0]
+
+        diff = tf.abs(predicted_value - original_value)
 
         # Apply threshold and square for stronger penalty
-        excess_penalty = tf.where(diff > self.threshold, 
+        excess_penalty = tf.where(diff > self.threshold,
                                   tf.square(diff - self.threshold),
                                   tf.zeros_like(diff))
 
@@ -119,46 +122,62 @@ class ConstraintLoss(tf.keras.losses.Loss):
     def get_config(self):
         config = super().get_config()
         config.update({
-            "original_values": self.original_values,
             "threshold": self.threshold,
         })
         return config
 
     @classmethod
     def from_config(cls, config: dict):
-        original_values = config.pop("original_values")
         threshold = config.pop("threshold")
-        return cls(original_values=original_values, threshold=threshold)
+        return cls(threshold=threshold)
 
 @keras.saving.register_keras_serializable(package="Octo", name="WeightedSumLoss")
 class WeightedSumLoss(tf.keras.losses.Loss):
     """Takes the weighted sum of two loss functions"""
-    def __init__(self, original_values, threshold = tf.convert_to_tensor(0.25), weight=tf.convert_to_tensor(0.99)):
+    def __init__(self, 
+                 threshold = tf.convert_to_tensor(0.25),
+                 weight=tf.convert_to_tensor(0.99),
+                 step=0,
+                 logwriter=None):
         super().__init__()
-        self.original_values = original_values
         self.threshold = threshold
-        self.f1 = ConstraintLoss(self.original_values)
+        self.f1 = ConstraintLoss()
         self.f2 = keras.losses.MeanSquaredError()
         self.w1 = weight
         self.w2 = tf.convert_to_tensor(1.0) - weight
+        if logwriter:
+            self.writer = logwriter
+        self.step = step
 
     def call(self, y_true, y_pred):
         loss1 = self.f1(y_true, y_pred)
-        loss2 = self.f2(y_true, y_pred)
+        loss2 = self.f2(y_true[0], y_pred)
         w_loss1 = self.w1 * loss1
         w_loss2 = self.w2 * loss2
+
+        if self.writer:
+            with self.writer.as_default():
+                tf.summary.scalar('w_loss1', w_loss1, step = self.step)
+                tf.summary.scalar('w_loss2', w_loss2, step = self.step)
+                tf.summary.scalar('loss1', loss1, step = self.step)
+                tf.summary.scalar('loss2', loss2, step = self.step)
+                self.writer.flush()
+
+        # loss_details = tf.convert_to_tensor([[w_loss1 + w_loss2, 
+        #                                      loss1, 
+        #                                      loss2, 
+        #                                      w_loss1, 
+        #                                      w_loss2]])
         return w_loss1 + w_loss2
 
     def get_config(self):
         config = super().get_config()
         config.update({
-            "original_values": self.original_values,
             "threshold": self.threshold,
         })
         return config
 
     @classmethod
     def from_config(cls, config: dict):
-        original_values = config.pop("original_values")
         threshold = config.pop("threshold")
-        return cls(original_values=original_values, threshold=threshold)
+        return cls(threshold=threshold)
